297519ad723feb54ce410a8680aa05e6f4659d30
Plamen Michev <plamen@voresforening.dk>
2025-12-04 18:43:41 +0100

    add small demo

--- 2-assignment/src/App.vue
+++ 2-assignment/src/App.vue
@@ -12,11 +12,51 @@
 
     <main class="main">
       <RouterView />
+      <button @click="play">Play first player</button>
+      <h1>Amount of players {{ game.playerCount }}</h1>
     </main>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { Game } from "./model/game";
+
+const currentRoundMemento = {
+  players: ["A", "B", "C"],
+  hands: [
+    [{ type: "WILD" }, { type: "DRAW", color: "GREEN" }],
+    [{ type: "NUMBERED", color: "RED", number: 7 }],
+    [{ type: "SKIP", color: "RED" }],
+  ],
+  drawPile: [{ type: "WILD DRAW" }],
+  discardPile: [
+    { type: "NUMBERED", color: "BLUE", number: 7 },
+    { type: "SKIP", color: "BLUE" },
+  ],
+  currentColor: "BLUE",
+  currentDirection: "clockwise",
+  dealer: 2,
+  playerInTurn: 1,
+};
+
+const unoMemento = {
+  players: ["A", "B", "C"],
+  currentRound: currentRoundMemento,
+  targetScore: 500,
+  scores: [220, 430, 80],
+  cardsPerPlayer: 7,
+};
+
+const game = ref(Game.createFromMemento(unoMemento));
+
+console.log("Game memento:", game.value.toMemento());
+
+function play() {
+  const playedCard = game.value.currentRound().play(0);
+  console.log("Played Card:", playedCard);
+}
+</script>
 
 <style scoped>
 #app {
---
+++ 2-assignment/src/model/game.ts
@@ -0,0 +1,176 @@
+import {
+  type Randomizer,
+  type Shuffler,
+  standardRandomizer,
+  standardShuffler,
+} from "../utils/random_utils";
+import { type Card } from "./deck";
+import { fromMemento, Round } from "./round";
+
+type GameConfig = {
+  players?: string[];
+  targetScore?: number;
+  cardsPerPlayer: number;
+  randomizer?: Randomizer;
+  shuffler?: Shuffler<Card>;
+};
+
+export class GameMemento {
+  players: string[] = [];
+  currentRound: any = undefined;
+  targetScore: number = 0;
+  scores: number[] = [];
+  cardsPerPlayer: number = 0;
+}
+
+export class Game {
+  private players: string[] = [];
+  private currentRoundModel: Round | undefined;
+  private targetScorePrivate: number = 0;
+  private scores: number[] = [];
+  private cardsPerPlayer: number = 0;
+  private randomizer: Randomizer = standardRandomizer;
+  private shuffler: Shuffler<Card> = standardShuffler;
+
+  constructor(gameConfig?: GameConfig) {
+    if (!gameConfig) {
+      return;
+    }
+
+    if (!gameConfig.players) {
+      gameConfig.players = ["A", "B"];
+    } else if (gameConfig.players.length < 2) {
+      throw new Error("A game must have at least two players.");
+    }
+
+    if (!gameConfig.targetScore && gameConfig.targetScore !== 0) {
+      gameConfig.targetScore = 500;
+    } else if (gameConfig.targetScore <= 0) {
+      throw new Error("Target score must be positive.");
+    }
+
+    this.players = [...gameConfig.players];
+    this.targetScorePrivate = gameConfig.targetScore;
+    this.scores = new Array(this.players.length).fill(0);
+    this.cardsPerPlayer = gameConfig.cardsPerPlayer;
+    this.randomizer = gameConfig.randomizer || standardRandomizer;
+    this.shuffler = gameConfig.shuffler || standardShuffler;
+
+    const dealerIndex = this.randomizer(this.players.length);
+    this.currentRoundModel = new Round(
+      this.players,
+      dealerIndex,
+      this.shuffler,
+      this.cardsPerPlayer
+    );
+    this.currentRoundModel.onEnd(this.onRoundFinished);
+  }
+
+  static createFromMemento(memento: any): Game {
+    if (memento.players.length < 2) {
+      throw new Error("A game must have at least two players.");
+    }
+
+    if (memento.targetScore <= 0) {
+      throw new Error("Target score must be positive.");
+    }
+
+    if (memento.scores.filter((s: any) => s < 0).length > 0) {
+      throw new Error("Scores must be non-negative.");
+    }
+
+    if (memento.scores.length != memento.players.length) {
+      throw new Error("Scores length must match players length.");
+    }
+
+    if (memento.scores.filter((s: any) => s > memento.targetScore).length > 1) {
+      throw new Error("There can be at most one winner.");
+    }
+
+    const game = new Game();
+    game.players = [...memento.players];
+    game.scores = [...memento.scores];
+    game.targetScorePrivate = memento.targetScore;
+    game.cardsPerPlayer = memento.cardsPerPlayer;
+
+    const winner = game.winner();
+    if (winner === undefined && memento.currentRound === undefined) {
+      throw new Error(
+        "An unfinished game must have a current round in the memento."
+      );
+    }
+
+    if (winner === undefined) {
+      game.currentRoundModel = fromMemento(
+        memento.currentRound,
+        undefined as any
+      );
+
+      game.currentRoundModel.onEnd(game.onRoundFinished);
+    }
+
+    return game;
+  }
+
+  player(index: number): string {
+    if (index < 0 || index >= this.players.length) {
+      throw new Error("Player index out of bounds.");
+    }
+
+    return this.players[index];
+  }
+
+  currentRound(): Round | undefined {
+    return this.currentRoundModel;
+  }
+
+  winner(): number | undefined {
+    for (let i = 0; i < this.scores.length; i++) {
+      if (this.scores[i] >= this.targetScore) {
+        return i;
+      }
+    }
+
+    return undefined;
+  }
+
+  score(playerIndex: number): number {
+    return this.scores[playerIndex];
+  }
+
+  get targetScore(): number {
+    return this.targetScorePrivate;
+  }
+
+  get playerCount(): number {
+    return this.players.length;
+  }
+
+  toMemento(): any {
+    return {
+      players: this.players,
+      currentRound: this.currentRoundModel?.toMemento(),
+      targetScore: this.targetScorePrivate,
+      scores: this.scores,
+      cardsPerPlayer: this.cardsPerPlayer,
+    };
+  }
+
+  private onRoundFinished = ({ winner }: { winner: number }) => {
+    this.scores[winner] += this.currentRoundModel?.score() || 0;
+    if (this.scores[winner] >= this.targetScore) {
+      this.currentRoundModel = undefined;
+    } else {
+      const dealerIndex = this.randomizer!(this.players.length);
+      this.currentRoundModel = new Round(
+        this.players,
+        dealerIndex,
+        this.shuffler,
+        this.cardsPerPlayer
+      );
+    }
+  };
+}
+
+export const createFromMemento = (memento: any): Game =>
+  Game.createFromMemento(memento);
